17209922e63bb54b125788985abde3097ac3d9ef
Fabiano1432 <fabiano.gocalves@etec.sp.gov.br>
2024-03-07 08:45:18 -0300

    pasta título

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './Title/';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Fabiano Henrique Sassi Gonçalves</Text>
       <Text>Helena Maria da Silva Costa</Text>
+      <Text>07 de março de 2024</Text>
+      <Title/>
       <StatusBar style="auto" />
     </View>
   );
